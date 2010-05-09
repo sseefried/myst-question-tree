6b659abb32e49bbfc3aef61293b0ae1fe44ff301
Sean Seefried <sean.seefried@gmail.com>
2010-05-09 18:55:09 +1000

    A lot of work on adding new responses, deleting and editing.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -33,7 +33,26 @@ class AdminController < ApplicationController
   
   # AJAX
   def add_response
+    q = Question.find(params[:id])
+    render :update do |page| 
+      page.insert_html :bottom, question_responses_css_id(q), 
+                       :partial => 'new_response', 
+                       :locals => { :question => q, :div_id => "response_#{usec_timestamp}" }
+    end
   end
+  
+  def new_response
+    r = Response.new(params[:response])
+    r[:question_id] = params[:question_id]
+    r.save
+    render :update do |page|
+      page.hide(params[:div_id])
+      page.insert_html :bottom, question_responses_css_id(r.question), 
+                       :partial => 'response_tree', :locals => { :response => r}
+    end
+  end
+  
+
   
   # AJAX
   def link_to_question
@@ -41,13 +60,35 @@ class AdminController < ApplicationController
 
   # AJAX
   def delete_question
-
-    # FIXME: Must make parent response become incomplete!
-
     q = Question.find(params[:id])
-    q.destroy
     render :update do |page|
-      page.replace_html question_css_id(q), :text => ''
+      page.replace_html response_css_id(q.response), :partial => 'incomplete_response', :locals => {:response => q.response }
+    end
+    q.destroy
+  end
+
+  #AJAX
+  def edit_question
+    q = Question.find(params[:id])
+    render :update do |page|
+      page.replace_html question_css_id(q), :partial => 'edit_question', :locals => { :question => q}
+    end
+  end
+  
+  # AJAX 
+  def show_question
+    q = Question.find(params[:id])
+    render :update do |page|
+      page.replace_html question_css_id(q), :partial => 'show_question', :locals => { :question => q}
+    end
+  end
+
+  # AJAX
+  def update_question
+    q = Question.find(params[:id])
+    q.update_attributes(params[:question])
+    render :update do |page|
+      page.replace_html question_css_id(q), :partial => 'show_question', :locals => { :question => q}
     end
   end
   
@@ -56,11 +97,29 @@ class AdminController < ApplicationController
   end
   
   #AJAX
-  def edit_question
+  def edit_response
+    r = Response.find(params[:id])
+    render :update do |page|
+      page.replace_html response_css_id(r), :partial => 'edit_response', :locals => { :response => r }
+    end
+  
   end
   
-  #AJAX
-  def edit_response
+  # AJAX 
+  def show_response
+    r = Response.find(params[:id])
+    render :update do |page|
+      page.replace_html response_css_id(r), :partial => 'show_response', :locals => { :response => r}
+    end
+  end
+  
+  # AJAX
+  def update_response
+    r = Response.find(params[:id])
+    r.update_attributes(params[:response])
+    render :update do |page|
+      page.replace_html response_css_id(r), :partial => 'show_response', :locals => { :response => r}      
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def response_text_css_id(response)
-    "response_text_#{response.id}"
+  def response_tree_css_id(response)
+    "response_tree_#{response.id}"
   end
 
   def response_css_id(response)
@@ -13,12 +13,27 @@ module ApplicationHelper
     "response_#{response.id}_result_#{result.id}"
   end
   
+  # This id is used for a div that contains the entire tree
+  def question_tree_css_id(question)
+    "question_tree_#{question.id}"
+  end
+  
+  # This id is used for a div that just contains the question and links
   def question_css_id(question)
     "question_#{question.id}"
   end
-  
+
+  def question_responses_css_id(question)
+    "question_responses_#{question.id}"
+  end
+
   def join_links(texts)
     texts.join(%Q{<span class="tiny-text">|</span>})
+  end
+
+  def usec_timestamp
+    t = Time.zone.now
+    "#{t.to_i}#{t.usec}"
   end
 
 end
