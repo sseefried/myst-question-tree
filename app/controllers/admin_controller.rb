62698b809e48f9a1bfda7389c8835ac56df90732
Sean Seefried <sean.seefried@gmail.com>
2010-09-18 19:13:08 +1000

    Fixed the editing of question, specifically +update_question+ and +show_question+ links

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,14 +8,14 @@ class AdminController < ApplicationController
     # FIXME: Show them all
     @question = Question.roots.first
   end
-  
+
   # AJAX
   def delete_result
     resp = Response.find(params[:id])
     results = resp.results
     result_to_delete = Result.find(params[:result_id])
     resp.results = results.select { |res| res.id != result_to_delete.id }
-    if resp.results.empty? 
+    if resp.results.empty?
       render :update do |page|
         page.replace_html response_css_id(resp), :partial => 'incomplete_response', :locals => { :response => resp}
       end
@@ -26,34 +26,34 @@ class AdminController < ApplicationController
       end
     end
   end
-  
+
   # AJAX
   def add_result
   end
-  
+
   # AJAX
   def add_response
     q = Question.find(params[:id])
-    render :update do |page| 
-      page.insert_html :bottom, question_responses_css_id(q), 
-                       :partial => 'new_response', 
+    render :update do |page|
+      page.insert_html :bottom, question_responses_css_id(q),
+                       :partial => 'new_response',
                        :locals => { :question => q, :div_id => "response_#{usec_timestamp}" }
     end
   end
-  
+
   def new_response
     r = Response.new(params[:response])
     r[:question_id] = params[:question_id]
     r.save
     render :update do |page|
       page.hide(params[:div_id])
-      page.insert_html :bottom, question_responses_css_id(r.question), 
+      page.insert_html :bottom, question_responses_css_id(r.question),
                        :partial => 'response_tree', :locals => { :response => r}
     end
   end
-  
 
-  
+
+
   # AJAX
   def link_to_question
   end
@@ -74,12 +74,12 @@ class AdminController < ApplicationController
       page.replace_html question_css_id(q), :partial => 'edit_question', :locals => { :question => q}
     end
   end
-  
-  # AJAX 
+
+  # AJAX
   def show_question
     q = Question.find(params[:id])
     render :update do |page|
-      page.replace_html question_css_id(q), :partial => 'show_question', :locals => { :question => q}
+      page.replace_html question_css_id(q), :partial => 'tree_branch', :locals => { :question => q}
     end
   end
 
@@ -88,37 +88,37 @@ class AdminController < ApplicationController
     q = Question.find(params[:id])
     q.update_attributes(params[:question])
     render :update do |page|
-      page.replace_html question_css_id(q), :partial => 'show_question', :locals => { :question => q}
+      page.replace_html question_css_id(q), :partial => 'tree_branch', :locals => { :question => q}
     end
   end
-  
+
   #AJAX
   def link_to_question
   end
-  
+
   #AJAX
   def edit_response
     r = Response.find(params[:id])
     render :update do |page|
       page.replace_html response_css_id(r), :partial => 'edit_response', :locals => { :response => r }
     end
-  
+
   end
-  
-  # AJAX 
+
+  # AJAX
   def show_response
     r = Response.find(params[:id])
     render :update do |page|
       page.replace_html response_css_id(r), :partial => 'show_response', :locals => { :response => r}
     end
   end
-  
+
   # AJAX
   def update_response
     r = Response.find(params[:id])
     r.update_attributes(params[:response])
     render :update do |page|
-      page.replace_html response_css_id(r), :partial => 'show_response', :locals => { :response => r}      
+      page.replace_html response_css_id(r), :partial => 'show_response', :locals => { :response => r}
     end
   end
 
