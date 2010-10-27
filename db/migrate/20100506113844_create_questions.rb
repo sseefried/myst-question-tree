782665b7776b88fbadab98875aefcac7a39135a6
Sean Seefried <sean.seefried@gmail.com>
2010-10-27 17:31:05 +1100

    Added textile field to Question model.

--- db/migrate/20100506113844_create_questions.rb
+++ db/migrate/20100506113844_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def self.up
     create_table :questions do |t|
       t.text :text, :null => false
+      t.text :textile
       t.references :response # if NULL then it's a root question
       t.timestamps
     end
